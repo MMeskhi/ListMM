4a37ed64feeb9a346eb677be4093f1fde8e320a9
Mikheil Meskhi <mishameskhi1@gmail.com>
2023-11-22 16:53:57 +0400

    framer motion test #3

--- components/intro.jsx
+++ components/intro.jsx
@@ -6,23 +6,26 @@ export default function Intro() {
   return (
     <h1 className="text-gray-300 text-4xl flex gap-4 mt-10">
       <motion.span
-        initial={{ scale: 0.24, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
+        initial={{ y: 24, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
         transition={{ delay: 0.1 }}
+        className="relative"
       >
         Watch
       </motion.span>
       <motion.span
-        initial={{ scale: 0.24, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
+        initial={{ y: -24, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
         transition={{ delay: 0.4 }}
+        className="relative"
       >
         Listen
       </motion.span>
       <motion.span
-        initial={{ scale: 0.24, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
+        initial={{ y: 24, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
         transition={{ delay: 0.7 }}
+        className="relative"
       >
         Play
       </motion.span>
