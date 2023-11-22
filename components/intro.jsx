9788bcabcb8af2bb2443e1beed5c49cd8663feea
Mikheil Meskhi <mishameskhi1@gmail.com>
2023-11-22 23:55:42 +0400

    removed middleware.js

--- components/intro.jsx
+++ components/intro.jsx
@@ -2,18 +2,16 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-export default function Intro(isVisible) {
+export default function Intro() {
   return (
     <h1 className="text-gray-300 text-4xl flex gap-4 mt-10">
-      {isVisible && (
-        <motion.span
-          initial={{ y: 24, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ delay: 0.1 }}
-        >
-          Watch
-        </motion.span>
-      )}
+      <motion.span
+        initial={{ y: 24, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.1 }}
+      >
+        Watch
+      </motion.span>
       <motion.span
         initial={{ y: -24, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
