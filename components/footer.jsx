21c02a0c10bb033e642bdedf836171e9c19def90
Mikheil Meskhi <mishameskhi1@gmail.com>
2023-11-22 17:25:47 +0400

    framer motion test #5

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function Footer() {
   const madeByBox = {
@@ -9,32 +9,30 @@ export default function Footer() {
   };
 
   return (
-    <AnimatePresence>
-      <footer className="container mx-auto px-28 xl:px-20 max-w-6xl mt-8 mb-4 max-sm:hidden flex justify-center items-center relative z-[102]">
-        <motion.div
-          className="flex flex-col justify-center items-center w-fit relative"
-          initial="default"
-          whileHover="hover"
+    <footer className="container mx-auto px-28 xl:px-20 max-w-6xl mt-8 mb-4 max-sm:hidden flex justify-center items-center relative z-[102]">
+      <motion.div
+        className="flex flex-col justify-center items-center w-fit relative"
+        initial="default"
+        whileHover="hover"
+      >
+        <motion.span
+          className="text-gray-300 text-sm absolute -top-36 border border-gray-950 pl-4 pr-16 whitespace-nowrap py-3 bg-gray-800 bg-opacity-60 backdrop-blur-xl rounded-md shadow-sm space-y-2 [&>*]:flex [&>*]:items-center [&>p>svg]:mr-2 pointer-events-none"
+          variants={madeByBox}
         >
-          <motion.span
-            className="text-gray-300 text-sm absolute -top-36 border border-gray-950 pl-4 pr-16 whitespace-nowrap py-3 bg-gray-800 bg-opacity-60 backdrop-blur-xl rounded-md shadow-sm space-y-2 [&>*]:flex [&>*]:items-center [&>p>svg]:mr-2 pointer-events-none"
-            variants={madeByBox}
-          >
-            <p>Next.js for code</p>
-            <p>Tailwind for UI</p>
-            <p>GitHub for versions</p>
-            <p>Vercel for host</p>
-          </motion.span>
-          <motion.div
-            className="text-gray-300 text-opacity-95 text-center cursor-default whitespace-nowrap select-none"
-            initial={{ y: 16, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ delay: 1 }}
-          >
-            &copy; 2023 Mikheil Meskhi
-          </motion.div>
+          <p>Next.js for code</p>
+          <p>Tailwind for UI</p>
+          <p>GitHub for versions</p>
+          <p>Vercel for host</p>
+        </motion.span>
+        <motion.div
+          className="text-gray-300 text-opacity-95 text-center cursor-default whitespace-nowrap select-none"
+          initial={{ y: 16, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 1 }}
+        >
+          &copy; 2023 Mikheil Meskhi
         </motion.div>
-      </footer>
-    </AnimatePresence>
+      </motion.div>
+    </footer>
   );
 }
